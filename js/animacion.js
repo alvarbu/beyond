ebaec749c43c5cc8a86d54da16caf3335f350228
alvarbu <74802594+alvarbu@users.noreply.github.com>
2021-11-17 12:56:03 -0600

    modificaciones

--- js/animacion.js
+++ js/animacion.js
@@ -1,8 +1,26 @@
-var prelo = document.getElementById('prelo');
+/*var prelo = document.getElementById('prelo');
 setTimeout(function() {
     prelo.classList.add('cerrar');
     prelo.style.opacity = '0';
 }, 1500);
+
+
+
+var prelo = document.getElementById('prelo');
+setTimeout(function() {
+    prelo.classList.add('cerrar');
+    prelo.style.opacity = '0';
+    prelo.classList.add('cerrar2');
+}, 1500)
+
+
+*/
+
+window.addEventListener('load', function(){
+    prelo.classList.add('cerrar');
+    prelo.style.opacity = '0';
+    prelo.classList.add('cerrar2');
+});
 
 window.sr = ScrollReveal(); {
     
